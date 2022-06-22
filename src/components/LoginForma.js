e189b007a244c0c3ec04c6b88cbdabbdd38636c4
Marija273 <maka.totovic@gmail.com>
2022-06-22 23:22:17 +0200

    Kontrolisanje input polja uz React Hooks useState i onChange

--- src/components/LoginForma.js
+++ src/components/LoginForma.js
@@ -1,12 +1,26 @@
+import {useState} from 'react';
+
 function LoginForma(){
+
+    const [korisnickoIme, setKorisnickoIme] = useState('');
+    const [lozinka, setLozinka] = useState('');
+
+    function updateKIme(e){
+        setKorisnickoIme(e.target.value);
+    }
+
+    function updateLoz(e){
+        setLozinka(e.target.value);
+    }
+    
     return (
         <div className="loginforma">
 
             <h4 id="username-h3">Korisničko ime</h4>
-            <input type="text" className="form-control" id="username-login" />  
+            <input type="text" className="form-control" id="username-login" value={korisnickoIme} onChange={updateKIme} />  
 
             <h4 id="password-h3">Lozinka</h4>
-            <input type="text" className="form-control" id="password-login" />
+            <input type="text" className="form-control" id="password-login" value={lozinka} onChange={updateLoz} />
 
             <button className="btn btn-primary" id="login-dugme">Prijavi se</button>
 
