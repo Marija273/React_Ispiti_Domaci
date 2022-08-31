8ae522a3c761e5d59e0987fb3da125c997207d87
Marija273 <maka.totovic@gmail.com>
2022-08-31 07:03:06 +0200

    U Login strani definisani studenti u sistemu

---
+++ src/strane/Login.js
@@ -0,0 +1,19 @@
+
+function Login() {
+
+    var studenti = [
+        { ime: 'Nikola', brojIndeksa: '1/18', korisnickoIme: '20180001', lozinka: '1', smer: 'ISIT' },
+        { ime: 'Jovana', brojIndeksa: '2/18', korisnickoIme: '201800023', lozinka: '2', smer: 'ISIT' },
+        { ime: 'Dragana', brojIndeksa: '3/18', korisnickoIme: '2018000', lozinka: '3', smer: 'ISIT' }
+    ]
+
+
+    return (
+        <div className="login">
+
+
+        </div>
+    );
+}
+
+export default Login;
